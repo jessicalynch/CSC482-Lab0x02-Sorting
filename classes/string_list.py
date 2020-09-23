de36eee1e93a5238de485fa8ea67b93cd741b71e
Jessica Lynch <jessica.lynch@trojans.dsu.edu>
2020-09-23 10:09:18 -0400

    Add verification tests

--- classes/string_list.py
+++ classes/string_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""integer_list.py: a class for generating integer lists easily"""
+"""string_list.py: a class for generating lists of strings"""
 __author__ = "Jessica Lynch"
 
 import random
@@ -22,19 +22,24 @@ class StringList:
         """
         self.values = []
         if file_path != None:
-            self.read_ints(file_path)
+            self.read_strings(file_path)
 
     # METHODS
     #---------------------------------------------
     def read_strings(self, file_path):        
         with open(file_path, "r") as f:
-            self.values = [str(x) for x in f]
+            self.values = [str(x).rstrip() for x in f]
 
     def gen_strings(self, N, k, min_v, max_v):
+        """Generate a list of strings"""
         if min_v < 1 or max_v > 126:
             raise ValueError("Out of ASCII range: use min and max values between 1 and 126")
         self.values = [''.join(chr(random.randint(min_v, max_v)) for _ in range(k)) for _ in range(N)]
-            
+
+    def generate_test_list(self, N, k, min_v, max_v):
+        """Generate a list of strings for testing sorting functions"""
+        self.gen_strings(N, k, min_v, max_v)
+
     def bubble_sort(self):
         """An elementary sorting algorithm"""
         self.values = sorts.bubble_sort(self.values)
@@ -61,5 +66,19 @@ class StringList:
         for i in range(len(L) - 1):
             if L[i+1] < L[i]:
                 return False
-
         return True
+
+    def verify_sorts(self, N, k, min_v, max_v):
+        """Verify that all sorts are working correctly"""
+        # Add all sorting functions to a list
+        sorting_functions = [self.bubble_sort, self.insertion_sort, self.selection_sort, self.merge_sort, self.quicksort]
+
+        # Generate a list of strings for each sorting function
+        # and test if the function sorts correctly
+        for sf in sorting_functions:
+            print(f"Generating list of length {N}, key width of {k}")
+            self.generate_test_list(N, k, min_v, max_v)
+            print(f"Sorting with {sf.__name__}")
+            sf()
+            print("Verifying...", "Sorted!" if self.is_sorted() else "Failed!", end="\n\n")
+
--- lab0x02.py
+++ lab0x02.py
@@ -9,16 +9,19 @@ from time import perf_counter_ns
 
 def main():
     try:
-        data = StringList()
-        data.gen_strings(5, 15, 97, 122)
-        print(data.values)
-        # data.bubble_sort()
-        # data.merge_sort()
-        # data.quicksort()
-        # data.insertion_sort()
-        data.selection_sort()
-        print(data.values)
-        print(data.is_sorted())
+        strings = StringList()
+        # strings.generate_test_list(5, 15, 97, 122)
+        strings.verify_sorts(5, 15, 97, 122)
+        strings2 = StringList("strings.txt")
+        print(strings2.values)
+        # print(strings.values)
+        # strings.bubble_sort()
+        # strings.merge_sort()
+        # strings.quicksort()
+        # strings.insertion_sort()
+        strings.selection_sort()
+        # print(strings.values)
+        # print(strings.is_sorted())
         
     except Exception as e:
         print(e)
